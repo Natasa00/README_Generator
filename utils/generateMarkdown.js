82eba5461fa9cfcdbd5d57b7a31c02c8a67074a4
Natasa <nkilibarda3@gmail.com>
2023-05-17 17:55:03 +1000

    Update license

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -5,19 +5,28 @@ function renderLicenseBadge(license) {
   return `![License](https://img.shields.io/badge/license-${license}-orange.svg)`;
 }
 
-// TODO: Create a function that returns the license link
+// A function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseLink(license) {
+  if (!license) return "";
+  return `https://opensource.org/license/${license}`;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
 ${renderLicenseBadge(data.license)}
+
+## Table of Contents
+
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
 
 ## Description
 
@@ -33,7 +42,7 @@ ${data.usage}
 
 ## License
 
-This project is under the ${data.license} license.
+This project is under the [${data.license} license](${renderLicenseLink(data.license)}).
 
 ## Contributing
 
